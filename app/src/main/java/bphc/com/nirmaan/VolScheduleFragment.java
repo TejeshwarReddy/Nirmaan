9a7ea961bf46d74189cb3dd5846d34eee7d210b0
Siddhant Kumar Patel <silu.patel1997@hotmail.com>
2017-02-05 14:54:29 +0530

    Volunteer Schedule Complete

--- app/src/main/java/bphc/com/nirmaan/VolScheduleFragment.java
+++ app/src/main/java/bphc/com/nirmaan/VolScheduleFragment.java
@@ -1,24 +1,32 @@
 package bphc.com.nirmaan;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.List;
+import java.util.Locale;
 
+import bphc.com.nirmaan.app.Constants;
 import bphc.com.nirmaan.database.DBTransactions;
 import bphc.com.nirmaan.object.VolSchedule;
 
 
 public class VolScheduleFragment extends Fragment {
 
-
     private LinearLayout mTutClassContainer;
+
+    private GregorianCalendar mCalendar;
 
     private DBTransactions mTransactions;
 
@@ -29,6 +37,7 @@ public class VolScheduleFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mTransactions = new DBTransactions(getActivity());
+        mCalendar = new GregorianCalendar();
     }
 
     @Override
@@ -44,27 +53,60 @@ public class VolScheduleFragment extends Fragment {
 
         LayoutInflater inflater = LayoutInflater.from(view.getContext());
         List<VolSchedule> schedules = mTransactions.getSchedule();
+        System.out.println("Size of schedule: " + schedules.size());
 
-/*        for (final VolSchedule schedule : schedules){
-            View scheduleView = inflater.inflate(R.layout.vol_schedule_row, mTutClassContainer);
+        for (final VolSchedule schedule : schedules){
+            View scheduleView = inflater.inflate(R.layout.vol_schedule_row, mTutClassContainer, false);
+            mCalendar.setTimeInMillis(schedule.getScheduledVisit());
 
-            TextView date = (TextView) view.findViewById(R.id.vol_schedule_date);
-            date.setText(schedule.getDate());
+            TextView date = (TextView) scheduleView.findViewById(R.id.vol_schedule_date);
+            date.setText(getDate(mCalendar));
 
-            TextView time_day = (TextView) view.findViewById(R.id.vol_schedule_accept);
-            time_day.setText(schedule.getTime() + ", " + schedule.getDay());
+            TextView time_day = (TextView) scheduleView.findViewById(R.id.vol_schedule_time_day);
+            time_day.setText(getTime(mCalendar) + ", " + getDay(mCalendar));
 
-            ImageView detail = (ImageView) view.findViewById(R.id.vol_schedule_detail);
+            ImageView detail = (ImageView) scheduleView.findViewById(R.id.vol_schedule_detail);
             detail.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     //TODO: Complete the intent
-                   startActivity(new Intent(getActivity(),QuestionBankActivity.class).putExtra(Constants.KEY_VOLUNTEER_TIME,schedule.getScheduledVisit()));
+                    final VolSchedule intentSchedule = schedule;
+                    Intent intent = new Intent(getActivity(), QuestionBankActivity.class);
+                    intent.putExtra(
+                            Constants.KEY_VOLUNTEER_TIME, intentSchedule.getScheduledVisit());
+                   startActivity(intent);
                 }
             });
-        }*/
+            mTutClassContainer.addView(scheduleView);
+        }
     }
 
+    /**
+     * @return Date as String, pattern: 8 February, 1997
+     */
+    public String getDate(Calendar calendar) {
+        int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+        String month = calendar.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.getDefault());
+        int year = calendar.get(Calendar.YEAR);
+        return Integer.toString(dayOfMonth) + " " + month + ", " + Integer.toString(year);
+    }
 
+    /**
+     * @return Day as String, pattern: Saturday
+     */
+    public String getDay(Calendar calendar) {
+        return calendar.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.LONG, Locale.getDefault());
+    }
+
+    /**
+     * @return Time as String, pattern: 9:15 AM
+     */
+    public String getTime(Calendar calendar) {
+
+        int hour = calendar.get(Calendar.HOUR);
+        int minute = calendar.get(Calendar.MINUTE);
+        String amPm = calendar.getDisplayName(Calendar.AM_PM, Calendar.LONG, Locale.getDefault());
+        return Integer.toString(hour) + ":" + Integer.toString(minute) + " " + amPm;
+    }
 
 }
