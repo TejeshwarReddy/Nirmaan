a6ce18fceb7c1bfe64fd369df4eb1719e033dd55
TejeshwarReddy <tejeshwarreddy1996@gmail.com>
2017-02-05 22:08:00 +0530

    updated DBTransactions

--- app/src/main/java/bphc/com/nirmaan/database/DBTransactions.java
+++ app/src/main/java/bphc/com/nirmaan/database/DBTransactions.java
@@ -2,6 +2,7 @@ package bphc.com.nirmaan.database;
 
 import android.content.Context;
 
+import bphc.com.nirmaan.object.StuAnswerListener;
 import bphc.com.nirmaan.object.StuMaterial;
 import bphc.com.nirmaan.object.StuMcq;
 import bphc.com.nirmaan.object.StuTopicCount;
@@ -66,6 +67,29 @@ public class DBTransactions {
         return realm.where(StuMcq.class)
                 .equalTo("subject",subject)
                 .equalTo("topic_id",topicid)
+                .findAll();
+    }
+
+    public void feedStudentAnswer(String answer, String subject,  int type, int topic_id, int question_id, int isRight){
+        Realm realm = Realm.getDefaultInstance();
+        realm.beginTransaction();
+        StuAnswerListener stuAnswerListener = realm.createObject(StuAnswerListener.class);
+        stuAnswerListener.setAnswer(answer);
+        stuAnswerListener.setIsRight(isRight);
+        stuAnswerListener.setQuestion_id(question_id);
+        stuAnswerListener.setSubject(subject);
+        stuAnswerListener.setType(type);
+        stuAnswerListener.setTopic_id(topic_id);
+        realm.commitTransaction();
+    }
+
+    public RealmResults<StuAnswerListener> getStudentAnswer(String subject, int topic_id, int question_id, int type){
+        realm = Realm.getDefaultInstance();
+        return realm.where(StuAnswerListener.class)
+                .equalTo("subject",subject)
+                .equalTo("topic_id",topic_id)
+                .equalTo("question_id",question_id)
+                .equalTo("type",type)
                 .findAll();
     }
 }
